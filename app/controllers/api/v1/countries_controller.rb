ea491050802b3b7e11d4ee2b39acdb3cbf11593e
Bohdan Vakh <Bohdanvah@gmail.com>
2023-11-27 23:00:36 +0200

    feat/add create method

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::CountriesController < Api::V1::ApiV1Controller
   def index
     @countries = Country.all
   end
+
+  def create
+    @country = Country.new(country_params)
+    if @country.save
+      render json: @country, status: :created
+    else
+      render json: "Country wasn't created :(", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def country_params
+    params.permit(:name, :name_eng, :code)
+  end
 end
