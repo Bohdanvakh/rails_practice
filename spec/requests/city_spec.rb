ba81209675dfc85650bfaeacf8991cb3dc86a6f4
Bohdan Vakh <Bohdanvah@gmail.com>
2023-12-19 22:36:53 +0200

    test/add tests for show a city

--- spec/requests/city_spec.rb
+++ spec/requests/city_spec.rb
@@ -45,4 +45,31 @@ RSpec.describe City, type: :request do
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
+
+  describe "GET #show" do
+    before do
+      @country = create(:country)
+      @city = create(:city, country: @country)
+
+      @customer = create(:customer, city: @city)
+      @employee = create(:employee)
+      @call_outcome = create(:call_outcome)
+      @call = create(:call, employee: @employee, call_outcome: @call_outcome, customer: @customer)
+    end
+    it "shows the city data" do
+      get "/api/v1/cities/#{@city.id}", as: :json
+
+      countries_json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:success)
+      expect(countries_json['id']).to eq(@city.id)
+      expect(countries_json['customers'][0]['name']).to eq(@customer.name)
+      expect(countries_json['customers'][0]['address']).to eq(@customer.address)
+      expect(countries_json['customers'][0]['calls']).to be_an(Array)
+      expect(countries_json['customers'][0]['calls'][0]['start_time']).not_to be_empty
+      expect(countries_json['customers'][0]['calls'][0]['end_time']).not_to be_empty
+      expect(countries_json['customers'][0]['calls'][0]['call_outcome']['id']).to eq(@call_outcome.id)
+      expect(countries_json['customers'][0]['calls'][0]['call_outcome']['outcome_text']).to eq(@call_outcome.outcome_text)
+    end
+  end
 end
