15a7c49addc23859d0efa689a5213c8d02ac0e38
Bohdan Vakh <Bohdanvah@gmail.com>
2023-12-19 21:52:40 +0200

    test/fixed factories

--- spec/factories/call_outcomes.rb
+++ spec/factories/call_outcomes.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :call_outcome do
-    outcome_text { Faker::String.random(length: 40..120) }
+    outcome_text { Faker::Lorem.sentence(word_count: 10) }
   end
 end
--- spec/factories/calls.rb
+++ spec/factories/calls.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :call do
-    name { Faker::Address.country }
     start_time  { Faker::Time.between(from: 1.year.ago, to: Time.zone.now, format: :default) }
     end_time { Faker::Time.between(from: 1.year.ago, to: Time.zone.now, format: :default) }
-    customer
     employee
     call_outcome
+    customer
   end
 end
